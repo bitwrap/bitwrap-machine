88a298052735202bfeb8d8f3343aa1efa98ab40a
stackdump <myork@stackdump.com>
2017-07-07 22:19:17 -0500

    rc0

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bitwrap-machine",
+    version="0.2.0",
+    author="Matthew York",
+    author_email="myork@stackdump.com",
+    description="Petri-Net State Machines",
+    license='MIT',
+    keywords='PNML petri-net state-machine bitwrap',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    long_description="""
+Bitwrap-machine provides a means of loading PNML documents as P/T net state machines.
+""",
+    url="http://getbitwrap.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Database :: Database Engines/Servers",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
